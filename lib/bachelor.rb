e89adec5c5c69173e8d6654d89aca22767db333e
Daniel Shimield <dxshim90@gmail.com>
2019-02-04 12:22:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,10 +41,18 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-   data.each do |season, names|
-     names.each do | ages|
-       newarr = [ages["age"].map { |x| }
-       
-       
-       
+age = 0
+count = 0
+  data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+        age += v.to_f
+      end
+    end
+  end
+
+  answer = (age/count).round
+return answer
+end
 end
